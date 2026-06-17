dc6480493e3b7e38be710db642fbccf86b928afc
rmagesto <nataliathomazelliferreira@gmail.com>
2026-06-17 08:20:24 -0300

    arrumando codigo

--- album.py
+++ album.py
@@ -3,66 +3,30 @@ from nodos import NodoLista
 
 class Album:
     def __init__(self):
-        self.cabeca = None
+        self.inicio = None
         self.repetidas = None
-        self.tamanho = 0
-        self.qtd_repetidas = 0
+        self.total = 0
+        self.total_repetidas = 0
 
     def adicionar(self, figurinha):
         if self.buscar_por_id(figurinha.id) is not None:
             self.adicionar_repetida(figurinha)
-            print("Figurinha repetida adicionada à lista de repetidas.")
-            return
-
-        novo = NodoLista(figurinha)
-
-        if self.cabeca is None:
-            self.cabeca = novo
+            print("Figurinha repetida adicionada.")
         else:
-            atual = self.cabeca
-            while atual.proximo is not None:
-                atual = atual.proximo
-            atual.proximo = novo
-
-        self.tamanho += 1
-        print("Figurinha adicionada ao álbum.")
+            novo = NodoLista(figurinha)
+            novo.proximo = self.inicio
+            self.inicio = novo
+            self.total += 1
+            print("Figurinha adicionada ao álbum.")
 
     def adicionar_repetida(self, figurinha):
         novo = NodoLista(figurinha)
-
-        if self.repetidas is None:
-            self.repetidas = novo
-        else:
-            atual = self.repetidas
-            while atual.proximo is not None:
-                atual = atual.proximo
-            atual.proximo = novo
-
-        self.qtd_repetidas += 1
-
-    def remover(self, id):
-        atual = self.cabeca
-        anterior = None
-
-        while atual is not None:
-            if atual.figurinha.id == id:
-                if anterior is None:
-                    self.cabeca = atual.proximo
-                else:
-                    anterior.proximo = atual.proximo
-
-                self.tamanho -= 1
-                print("Figurinha removida.")
-                return True
-
-            anterior = atual
-            atual = atual.proximo
-
-        print("Figurinha não encontrada.")
-        return False
+        novo.proximo = self.repetidas
+        self.repetidas = novo
+        self.total_repetidas += 1
 
     def buscar_por_id(self, id):
-        atual = self.cabeca
+        atual = self.inicio
 
         while atual is not None:
             if atual.figurinha.id == id:
@@ -72,7 +36,7 @@ class Album:
         return None
 
     def buscar_por_nome(self, nome):
-        atual = self.cabeca
+        atual = self.inicio
         encontrou = False
 
         while atual is not None:
@@ -82,10 +46,10 @@ class Album:
             atual = atual.proximo
 
         if not encontrou:
-            print("Nenhuma figurinha encontrada com esse nome.")
+            print("Nenhuma figurinha encontrada.")
 
     def buscar_por_pais(self, pais):
-        atual = self.cabeca
+        atual = self.inicio
         encontrou = False
 
         while atual is not None:
@@ -95,14 +59,34 @@ class Album:
             atual = atual.proximo
 
         if not encontrou:
-            print("Nenhuma figurinha encontrada dessa seleção.")
+            print("Nenhuma figurinha encontrada.")
+
+    def remover(self, id):
+        atual = self.inicio
+        anterior = None
+
+        while atual is not None:
+            if atual.figurinha.id == id:
+                if anterior is None:
+                    self.inicio = atual.proximo
+                else:
+                    anterior.proximo = atual.proximo
+
+                self.total -= 1
+                print("Figurinha removida.")
+                return
+
+            anterior = atual
+            atual = atual.proximo
+
+        print("Figurinha não encontrada.")
 
     def mostrar_album(self):
-        if self.cabeca is None:
+        if self.inicio is None:
             print("Álbum vazio.")
             return
 
-        atual = self.cabeca
+        atual = self.inicio
 
         while atual is not None:
             print(atual.figurinha.exibir())
@@ -120,13 +104,10 @@ class Album:
             atual = atual.proximo
 
     def contar_repetidas(self):
-        return self.qtd_repetidas
+        return self.total_repetidas
 
     def porcentagem_completa(self, total_figurinhas):
-        if total_figurinhas <= 0:
-            return 0
-
-        return (self.tamanho / total_figurinhas) * 100
+        return (self.total / total_figurinhas) * 100
 
     def possui_repetida(self, id):
         atual = self.repetidas
@@ -151,7 +132,7 @@ class Album:
                 else:
                     anterior.proximo = atual.proximo
 
-                self.qtd_repetidas -= 1
+                self.total_repetidas -= 1
                 return figurinha
 
             anterior = atual
@@ -161,7 +142,7 @@ class Album:
 
     def para_lista_dicionarios(self):
         dados = []
-        atual = self.cabeca
+        atual = self.inicio
 
         while atual is not None:
             dados.append(atual.figurinha.para_dicionario())
